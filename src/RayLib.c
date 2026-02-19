4c59c30d2b5cdc7342a5f141e12c5073229da8d3
JakobUnger-freebird <TheSkriptKid@proton.me>
2026-02-19 23:58:23 +0100

    It now stores the LevelCnt where u at

--- src/RayLib.c
+++ src/RayLib.c
@@ -11,9 +11,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdarg.h> //Infinit arguments, Infinit Power
+#include <sys/stat.h>
+#include <string.h>
 #include "raylib.h"
 #include "raymath.h"
-#include <stdarg.h> //Infinit arguments, Infinit Power
+
 
 enum Direction{
 	up,
@@ -38,8 +41,13 @@ Rectangle GetClosestRect(Rectangle Player, enum Direction direction, int amount,
 
 //Level Loops
 int MenuLoop(void);
+
+int Level0Loop(void);
+
 int Level1Loop(void);
+int Level1_2Loop();
 int Level2Loop(void);
+int Level2_2Loop(void);
 int Level3Loop(void);
 int Level4Loop(void);
 int Level5Loop(void);
@@ -53,9 +61,15 @@ Texture2D texture;
 Texture2D star;
 
 
-int frame = 0;
-float timer = 0.0f;
 
+float timer = 900.0f;
+int timer_0 = 0.0f;
+bool done_0 = true;
+bool firstrungame = true;
+int nextlvl = -1;
+char filepath[512];
+
+FILE *file;
 
 int main(void)
 {
@@ -73,62 +87,206 @@ int main(void)
 	star = LoadTexture("images/Star.png");
 
 
+	//Saves Logik:
+	char *home = getenv("HOME");
+	if (home != NULL) {
+		char path[512];
+		char helperpath[512];
+		int res =0;
+		snprintf(helperpath, sizeof(helperpath),"%s/.GetToSpace", home);
+		snprintf(path, sizeof(path), "%s/.GetToSpace/saves", home);
+		snprintf(filepath, sizeof(filepath), "%s/.GetToSpace/saves/save.dat", home);
+
+		res = mkdir(helperpath, 0755);
+		if(res == -1){
+			firstrungame = false;
+		}
+		res = mkdir(path, 0775);
+		if(res == -1){
+			firstrungame = false;
+		}
+
+		file = fopen(filepath, "a+");
+		if (file == NULL) {
+			perror("Error: Couldn't open saves file");
+		}
+	}
+
+	if(file !=0 && firstrungame){
+		fseek(file, 0, SEEK_SET);
+		fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n0");
+		fflush(file);
+	}
+
+	if(file !=0){
+		fseek(file, 0, SEEK_SET);
+		fscanf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", &nextlvl);
+		nextlvl = nextlvl/298;
+	}
 
 
-
-	int levelcnt = 1;
+	int levelcnt = 98;
 	// Game loop
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 
-		DrawTexture(texture, 0, 0, WHITE);
+		if(levelcnt != 0){
+			DrawTexture(texture, 0, 0, WHITE);
+		}
+
+		char levelText[32];
+		snprintf(levelText, sizeof(levelText), "%d", nextlvl);
+		DrawText(levelText, 100, 30, 10, RED);
 
 		switch (levelcnt) {
-		case 0:
+		case 98:
 			levelcnt = MenuLoop();
+			break;
+		case 0:
+			if(Level0Loop() == 1){
+				levelcnt++;
+				ClearBackground(RAYWHITE);
+				PlayerPosition.x = 150;
+				PlayerPosition.y = 500;
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
+			}
 			break;
 		case 1:
 			if(Level1Loop() == 1){
 				levelcnt++;
 				ClearBackground(RAYWHITE);
+				PlayerPosition.x = 750;
+				PlayerPosition.y = 500;
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
 			}
 			break;
 		case 2:
-			if(Level2Loop() == 1){
+			if(Level1_2Loop() == 1){
 				levelcnt++;
 				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
 			}
 			break;
 		case 3:
-			if(Level3Loop() == 1){
+			if(Level2Loop() == 1){
 				levelcnt++;
 				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
 			}
 			break;
 		case 4:
-			if(Level4Loop() == 1){
+			if(Level2_2Loop() == 1){
 				levelcnt++;
 				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
 			}
 			break;
 		case 5:
-			if(Level5Loop() == 1){
+			if(Level3Loop() == 1){
 				levelcnt++;
 				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
 			}
 			break;
 		case 6:
+			if(Level4Loop() == 1){
+				levelcnt++;
+				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
+			}
+			break;
+		case 7:
+			if(Level5Loop() == 1){
+				levelcnt++;
+				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
+			}
+			break;
+		case 8:
 			if(Level6Loop() == 1){
 				levelcnt++;
 				ClearBackground(RAYWHITE);
+				freopen(filepath, "w", file);
+				freopen(filepath, "a+", file);
+				if(file !=0){
+					fseek(file, 0, SEEK_SET);
+					fprintf(file, "WARNING: EDITING THIS FILE MAY CURROPT YOUR GAME!\n%i", levelcnt * 298);
+					fflush(file);
+				}else{
+					perror("ERROR: Resetting file");
+				}
 			}
 			break;
 		case 99:
 			levelcnt = CreditsLoop();
 			break;
 		default:
-			levelcnt = 0;
+			levelcnt = 98;
 			break;
 		}
 
@@ -141,6 +299,7 @@ int main(void)
 
 	UnloadTexture(texture);
 	UnloadTexture(star);
+	fclose(file);
 
 	CloseWindow();
 
@@ -168,7 +327,7 @@ int MenuLoop(void){
 		DrawText("Credits", 390, 825, 50, BLUE);
 
 		if(clicked){
-			return 1;
+			return nextlvl;
 		}
 
 	} else if(CheckCollisionPointRec(MousePos, Credits)){
@@ -195,7 +354,84 @@ int MenuLoop(void){
 
 	ClearBackground(LIGHTGRAY);
 
-	return 0;
+	return 98;
+}
+
+int Level0Loop(void){
+
+	timer ++;
+	timer_0 = (int)(timer / 60);
+
+	switch (timer_0) {
+	case 1:
+		DrawText("Space.", 480, 500, 20 , GREEN);
+		break;
+	case 4:
+		ClearBackground(BLACK);
+		DrawText("What is Space?", 460, 500, 20 , GREEN);
+		break;
+	case 7:
+		ClearBackground(BLACK);
+		DrawText("Or better, why are we so Fascinated of it?", 350, 500, 20 , GREEN);
+		break;
+	case 10:
+		ClearBackground(BLACK);
+		DrawText("Why do we want to get to it? Explore it? Look at it?", 250, 500, 20 , GREEN);
+		break;
+	case 13:
+		ClearBackground(BLACK);
+		DrawText("I dont know. ", 440, 500, 20 , GREEN);
+		break;
+	case 15:
+		ClearBackground(BLACK);
+		DrawText("This is Cube.", 440, 500, 20 , GREEN);
+		PlayerPosition.x = 490;
+		PlayerPosition.y = 700;
+		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
+		break;
+	case 16:
+		ClearBackground(BLACK);
+		DrawText("Say Hi Cube!", 440, 500, 20 , GREEN);
+		if(done_0){
+			velocityY = -25;
+			done_0 = false;
+		}
+
+		break;
+	case 18:
+		ClearBackground(BLACK);
+		DrawText("He also loves Space!", 440, 500, 20 , GREEN);
+		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
+		break;
+	case 20:
+		ClearBackground(BLACK);
+		DrawText("Can you help him get to space? I think a good Vehicle is a few Levels to the right!", 80, 500, 20 , GREEN);
+		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
+		break;
+
+
+
+	case 22:
+		PlayerPosition.x = 150;
+		PlayerPosition.y = 500;
+		return 1;
+		break;
+	default:
+		break;
+	}
+
+
+	if(timer_0 >= 16 && timer_0 <= 18){
+		jump = jump - (jump * 0.1f);
+		velocityY = velocityY - velocityY *0.1f + gravity;
+		if(PlayerPosition.y > 700){
+			velocityY=0;
+		}
+		PlayerPosition.y += velocityY;
+		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
+	}
+
+	return -1;
 }
 
 int Level1Loop(void){
@@ -222,6 +458,103 @@ int Level1Loop(void){
 
 
 		DrawText("Look at the Stars. Aren't they Beatiful? I wanna try to get to them!", 300, 700, 15 , GREEN);
+
+		if(PlayerPosition.x ==150){
+			timer ++;
+			if(timer > 300){
+				DrawText("Move with  W A D", 300, 600, 20 , GRAY);
+			}
+		}else{
+			timer = 0;
+		}
+
+		if(IsKeyDown(KEY_D)){
+			velocityX = Clamp(velocityX + 1, -5, 5);
+		}else if(IsKeyDown(KEY_A)){
+			velocityX = Clamp(velocityX - 1, -5, 5);
+		}
+
+
+		if(IsKeyPressed(KEY_W) && Nogravity){
+			velocityY = -25;
+		}
+
+		Nogravity = false;
+		if(CheckCollisionRecs(startplatt, PlayerPosition)){
+			Nogravity = true;
+			if(velocityY > 0){
+				velocityY = 0;
+			}
+		}
+
+
+
+		velocityX = velocityX - velocityX *0.1f;
+		if(!IsKeyDown(KEY_W)){
+			jump = jump - (jump * 0.1f);
+		}
+
+
+		if(Nogravity){
+			velocityY = velocityY - velocityY *0.1f;
+			PlayerPosition.y += velocityY + jump;
+		}else{
+			velocityY = velocityY - velocityY *0.1f + gravity;
+			PlayerPosition.y += velocityY;
+		}
+
+		PlayerPosition.x += velocityX;
+
+
+		// Movement
+		velocityX *= 0.9f;
+		if(!Nogravity){
+			velocityY += gravity;
+		}
+		velocityY *= 0.98f;
+
+
+		if ((PlayerPosition.y + PlayerPosition.height) > Death.y){ //Death
+			PlayerPosition.x = 450;
+			PlayerPosition.y = 500;
+		}
+
+		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
+	}
+	return -1;
+}
+
+int Level1_2Loop(void){
+	//Objekts
+	Rectangle startplatt = {50, 850,900, 25};
+	Rectangle Goal = {150, 750, 25,25};
+
+	DrawRectangleLines(startplatt.x,startplatt.y,startplatt.width,startplatt.height, BLACK);
+	DrawRectangle(Death.x, Death.y, Death.width, Death.height, RED);
+	DrawRectangleRec(Goal, GOLD);
+
+
+	DrawTexture(star, Goal.x - Goal.width, Goal.y-Goal.height, WHITE);
+
+
+	if(CheckCollisionRecs(PlayerPosition, Goal)){
+		PlayerPosition.x = 150;
+		PlayerPosition.y = 500;
+		return 1;
+	} else{
+		DrawFPS(50, 50);
+
+
+		DrawText("They are left, right, above, below... EVERYWHERE!!", 300, 700, 15 , GREEN);
+
+		if(PlayerPosition.x ==150){
+			timer ++;
+			if(timer > 300){
+				DrawText("Move with  W A D", 300, 600, 20 , GRAY);
+			}
+		}else{
+			timer = 0;
+		}
 
 		if(IsKeyDown(KEY_D)){
 			velocityX = Clamp(velocityX + 1, -5, 5);
@@ -295,6 +628,15 @@ int Level2Loop(void){
 	DrawTexture(star, Goal.x - Goal.width, Goal.y-Goal.height, WHITE);
 
 
+	if(PlayerPosition.y >= 825){
+		timer ++;
+		if(timer > 300){
+			DrawText("Jump with Space", 300, 600, 20 , GRAY);
+		}
+	}else {
+		timer = 0;
+	}
+
 	if(CheckCollisionRecs(PlayerPosition, Goal)){
 		PlayerPosition.x = 150;
 		PlayerPosition.y = 500;
@@ -365,6 +707,103 @@ int Level2Loop(void){
 		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
 
 		ClearBackground(RAYWHITE);
+	}
+	return -1;
+}
+
+int Level2_2Loop(void){
+	//Objekts
+	Rectangle startplatt = {50, 850,200, 25};
+	Rectangle startplatt2 = {350, 850,200, 25};
+	Rectangle startplatt3 = {650, 850,200, 25};
+	Rectangle Goal = {770, 750, 25,25};
+
+
+
+	DrawRectangleLines(startplatt.x,startplatt.y,startplatt.width,startplatt.height, BLACK);
+	DrawRectangleLines(startplatt2.x,startplatt2.y,startplatt2.width,startplatt2.height, BLACK);
+	DrawRectangleLines(startplatt3.x,startplatt3.y,startplatt3.width,startplatt3.height, BLACK);
+	DrawRectangle(Death.x, Death.y, Death.width, Death.height, RED);
+	DrawRectangleRec(Goal, GOLD);
+
+
+	DrawTexture(star, Goal.x - Goal.width, Goal.y-Goal.height, WHITE);
+
+
+	if(CheckCollisionRecs(PlayerPosition, Goal)){
+		PlayerPosition.x = 150;
+		PlayerPosition.y = 500;
+		return 1;
+	} else{
+		DrawFPS(50, 50);
+
+
+		DrawText("No Obstacles stand strong in the way", 300, 700, 15 , GREEN);
+
+
+
+		if(IsKeyDown(KEY_D)){
+			velocityX = Clamp(velocityX + 1, -5, 5);
+		}else if(IsKeyDown(KEY_A)){
+			velocityX = Clamp(velocityX - 1, -5, 5);
+		}
+
+
+		if(IsKeyPressed(KEY_W) && Nogravity){
+			velocityY = -25;
+		}
+
+		Nogravity = false;
+		if(CheckCollisionRecs(startplatt, PlayerPosition)){
+			Nogravity = true;
+			if(velocityY > 0){
+				velocityY = 0;
+			}
+		} else if(CheckCollisionRecs(startplatt2, PlayerPosition)){
+			Nogravity = true;
+			if(velocityY > 0){
+				velocityY = 0;
+			}
+		} else if(CheckCollisionRecs(startplatt3, PlayerPosition)){
+			Nogravity = true;
+			if(velocityY > 0){
+				velocityY = 0;
+			}
+		}
+
+
+
+		velocityX = velocityX - velocityX *0.1f;
+		if(!IsKeyDown(KEY_W)){
+			jump = jump - (jump * 0.1f);
+		}
+
+
+		if(Nogravity){
+			velocityY = velocityY - velocityY *0.1f;
+			PlayerPosition.y += velocityY + jump;
+		}else{
+			velocityY = velocityY - velocityY *0.1f + gravity;
+			PlayerPosition.y += velocityY;
+		}
+
+		PlayerPosition.x += velocityX;
+
+
+		// Movement
+		velocityX *= 0.9f;
+		if(!Nogravity){
+			velocityY += gravity;
+		}
+		velocityY *= 0.98f;
+
+
+		if ((PlayerPosition.y + PlayerPosition.height) > Death.y){ //Death
+			PlayerPosition.x = 450;
+			PlayerPosition.y = 500;
+		}
+
+		DrawRectangleGradientEx(PlayerPosition, GREEN, BLUE, PURPLE, GRAY);
 	}
 	return -1;
 }
@@ -490,11 +929,10 @@ int Level4Loop(void){
 	DrawRectangleLines(startplatt.x,startplatt.y,startplatt.width,startplatt.height, BLACK);
 	DrawRectangle(Death.x, Death.y, Death.width, Death.height, RED);
 
-	DrawRectangleRec(jump1, BLACK);
-	DrawRectangleRec(jump2, BLACK);
-	DrawRectangleRec(jump3, BLACK);
-	DrawRectangleRec(jump5, BLACK);
-
+	DrawRectangleLines(jump1.x,jump1.y,jump1.width,jump1.height, GRAY);
+	DrawRectangleLines(jump2.x,jump2.y,jump2.width,jump2.height, GRAY);
+	DrawRectangleLines(jump3.x,jump3.y,jump3.width,jump3.height, GRAY);
+	DrawRectangleLines(jump5.x,jump5.y,jump5.width,jump5.height, GRAY);
 
 	DrawRectangleRec(Goal, GOLD);
 
@@ -700,6 +1138,15 @@ int Level6Loop(void){
 
 	DrawTexture(star, Goal.x - Goal.width, Goal.y-Goal.height, WHITE);
 
+	if(PlayerPosition.y > 550){
+		timer ++;
+		if(timer > 300){
+			DrawText("Try standing still in the blue Boost Pad and Press Jump!", 300, 500, 15 , GRAY);
+		}
+	}else{
+		timer =0;
+	}
+
 	if(CheckCollisionRecs(PlayerPosition, Goal)){
 		PlayerPosition.x = 150;
 		PlayerPosition.y = 500;
@@ -708,7 +1155,7 @@ int Level6Loop(void){
 		DrawFPS(50, 50);
 
 
-		DrawText("Are you prepared?", 300, 700, 15 , GREEN);
+		DrawText("Are you prepared?", 400, 700, 15 , GREEN);
 
 
 		if(IsKeyDown(KEY_D)){
@@ -802,7 +1249,7 @@ int CreditsLoop(void){
 		DrawRectangleRounded(Back, 0.3, 10, YELLOW);
 		DrawText("Back", 440, 790, 25, BLUE);
 		if(clicked){
-			return 0;
+			return 98;
 		}
 	}else{
 		DrawRectangleRounded(Back, 0.3, 10, PURPLE);
